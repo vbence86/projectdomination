f4ca1017a41c6943c32116402b2f1ecfd1b294f7
Bence Varga <vbence86@gmail.com>
2015-11-08 23:57:32 +0000

    Installing Google Map module into the MapCtrl object

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,8 +41,45 @@ angular.module('pd.controllers', [])
   };
 })
 
-.controller('MapCtrl', function($scope) {
-  
+.controller('MapCtrl', function($scope, $ionicLoading, $compile) {
+    function initialize() {
+      var myLatlng = new google.maps.LatLng(43.07493,-89.381388),
+      
+        mapOptions = {
+          center: myLatlng,
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        },
+
+        map = new google.maps.Map(document.getElementById("google-map-container"), mapOptions);
+
+      $scope.map = map;
+      $scope.centerOnMe();
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+    
+    $scope.centerOnMe = function() {
+      if(!$scope.map) {
+        return;
+      }
+
+      $scope.loading = $ionicLoading.show({
+        content: 'Getting current location...',
+        showBackdrop: false
+      });
+
+      navigator.geolocation.getCurrentPosition(function(pos) {
+
+        $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        $ionicLoading.hide();
+
+      }, function(error) {
+        alert('Unable to get location: ' + error.message);
+      });
+
+    };
+    
 });
 
 
